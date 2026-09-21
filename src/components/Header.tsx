5991d366ba5360253f22c605a623c99f0647ae91
EricaMelodi <gusgeraer@student.gu.se>
2026-09-21 19:27:38 +0200

    add temp logo and import it in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import { Link } from '@tanstack/react-router'
 import ThemeToggle from './ThemeToggle'
+import logo from '../public/logo.jpeg'
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 border-b border-[var(--line)] bg-[var(--header-bg)] px-4 backdrop-blur-lg">
       <nav className="page-wrap flex flex-wrap items-center gap-x-3 gap-y-2 py-3 sm:py-4">
+        {/* <img src={logo} alt="Logo" /> */}
         <h2 className="m-0 flex-shrink-0 text-base font-semibold tracking-tight">
           <Link
             to="/"
